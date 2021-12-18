e8cc8db7bdd548c3ac6a3897c510f816e9a09cfa
Yuri Willian <yuriwilian@gmail.com>
2021-12-18 14:37:47 -0300

    AnalisadorDeNumeros

---
+++ Readme.md
@@ -0,0 +1,12 @@
+Olá pessal, tudo bem? :D
+
+Esse site foi desenvolvido para analisar números armazenados em um array chamado "dados[]". Temos dois botões um input e uma lista com scroll onde será adicionado os números quando clicar em adicionar.
+
+O segundo botã é o "ANALISAR", a função dele obviamente é analisar os números e inserir as respostas via ".innerText" no parágrafo em baixo do botão analisar.
+
+Ele trará as seguintes respostas:
+    "Ao todo temos X números cadastrados."
+    "O maior valor informado foi X"
+    "O menor valor informado foi X"
+    "Somando todos os valores temos X"
+    "A média dos valores digitados é X"
---
+++ script.js
@@ -0,0 +1,49 @@
+let dados = []
+
+
+
+function adicionar() {
+    var número = document.getElementById("número")
+    if (número.value.length == 0) {
+        window.alert("Adicione um número")
+    } else if (número.value > 100) {
+        window.alert("O número deve ser entre 1 e 100.")
+    } else {
+    var lista = document.getElementById("scroll")
+    var númeropreenchido = Number(número.value)
+    dados.push(númeropreenchido)
+    lista.innerText += `\nO número digitado foi ${númeropreenchido}`
+}}
+
+
+
+function analisar() {
+    if (número.value.length == 0) {
+        window.alert("Adicione um número")
+    } else {
+    // "Ao todo temos X números cadastrados."
+    resposta = document.getElementById("resposta")
+    resposta.innerText = `Ao todo temos ${dados.length} números cadastrados`
+
+    // "O maior valor informado foi X"
+    dados.sort(function(a, b) {
+        return b - a;
+    });
+    resposta.innerText +=`\nO maior valor informado foi ${dados[0]}`
+
+    // "O menor valor informado foi X"
+    dados.sort()
+    resposta.innerText +=`\n O menor valor informado foi ${dados[0]}`
+
+    // "Somando todos os valores temos X"
+    var soma = 0;
+        for(var indice = 0; indice < dados.length; indice++) {
+            soma += dados[indice];
+        }
+        resposta.innerText += `\nSomando todos os valores temos ${soma}`
+    
+    // "A média dos valores digitados é X"
+    var média = soma / dados.length
+    resposta.innerText += `\nA média dos valores digitados é ${média}`
+
+}}
